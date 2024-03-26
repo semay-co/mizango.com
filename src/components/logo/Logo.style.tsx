1ec8d530a5556523e9f240385fdab25ccf33a005
Ameer Nuri <53400923+ameernuri@users.noreply.github.com>
2024-03-26 11:18:25 +0300

    fix styled components typing error

--- src/components/logo/Logo.style.tsx
+++ src/components/logo/Logo.style.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-const Styled = styled.svg<{ height?: number }>`
+const Styled = styled.svg<{ height?: string | number | undefined }>`
   svg {
     height: ${(props) => props.height};
   }
